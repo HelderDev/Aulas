3d47e2572b93692ba45c942b860b9c61a4719b18
helder cavalcante <hdrhelder7@hotmail.com>
2019-11-30 21:04:29 -0300

    commit inicial

---
+++ Loja/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View("Login");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult UsuarioAdicionado(Usuario u)
+        {
+            
+            UsuarioDAO uu = new UsuarioDAO();
+            uu.Inserir(u.Nome, u.Funcao);
+
+            //uu.Listar();
+        
+            return View(uu.Listar());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Loja/Models/UsuarioDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Models
+{
+    public class UsuarioDAO
+    {
+        SqlConnection conn;
+
+        public void Inserir(string nome, string funcao)
+        {
+            conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DBLoja"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("insert into usuario values (@name, @funcao)");
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@name", nome);
+            cmd.Parameters.AddWithValue("@funcao", funcao);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public IEnumerable<Usuario> Listar()
+        {
+            conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DBLoja"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("select * from usuario");
+            conn.Open();
+            // cmd.ExecuteNonQuery();
+            cmd.Connection = conn;
+            List<Usuario> usuarios = new List<Usuario>();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Usuario u = new Usuario();
+                u.Nome = (string)reader["nome"];
+                u.Funcao = (string)reader["funcao"];
+                usuarios.Add(u);
+            }
+            return usuarios;
+        }
+    }
+}
